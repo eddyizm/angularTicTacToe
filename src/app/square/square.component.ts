8459cefb55385ca65be1835e191ebc274a9a51b9
eddyizm <eddyizm@gmail.com>
2020-03-07 09:06:26 -0800

    saved the day, fixed typos and squashed the bugs!

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,19 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   template: `
-    <p>
-      {{ rando }}
-    </p>
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+    
   `,
-  styles: []
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
+
 export class SquareComponent {
-  rando; // = Math.random()
-  constructor()
-  {
-    setInterval(() => this.rando = Math.random(), 500 )
-  } 
+  
+  @Input() value: 'X' | 'O';
 
 }
